d4106d13c07fecbce51e4139d84b5ab8fa9d1e5a
Cesar Augusto <cesinhalius@hotmail.com>
2024-05-02 19:32:58 -0300

    Atualizando arquivos

--- Desafios/ToDo_List/index.js
+++ Desafios/ToDo_List/index.js
@@ -1,36 +1,66 @@
-function TarefaConcluida(cb){
-    const tarefa = cb.closest('li');
-    tarefa.remove();
-}
+const botao = document.querySelector('.button-add-task');
+const campo = document.querySelector('.input-task');
+const lista = document.querySelector('.list-task');
 
 
+let minhaListaDeItens = [];
 
 
-function addTask(e){
-    e.preventDefault();
-    const inputTask = e.target.task;
-    const textTask = inputTask.value.trim();
-
-    if(textTask === ' '){
-        alert('A tarefa precisa de um nome');
+function adicionarNovaTarefa(){
+    if(campo.value === ''){
+        alert("Digite algo para Adicionar")
         return;
     }
-   
     
-    const htmlTask =`
-        <li>
-            <input type="checkbox" onclick="finalizarTarefa(this)" />
-            <span>${textTask}</span>
-        </li>
-    
-    `;
+    minhaListaDeItens.push({
+        tarefa:campo.value,
+        concluida: false,
+    })
 
-    document
-    .querySelector('.todo-list')
-    .insertAdjacentHTML('beforeend', htmlTask.trim());
+    campo.value = ''
 
-    inputTask.value = ' ';
-    
+    mostrarTarefa()
 }
 
-document.querySelector('.add-todo').addEventListener('submit', addTask);
+function mostrarTarefa(){
+    let novoItem = ''
+
+    minhaListaDeItens.forEach((item, posicao) => {
+        novoItem = novoItem + `
+        <li class="task ${item.concluida && 'done'}">
+            <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
+            <p>${item.tarefa}</p>
+            <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarTarefa(${posicao})">
+        </li>
+        `
+    })
+
+    lista.innerHTML = novoItem;
+
+    localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+}
+
+function concluirTarefa(posicao){
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+    mostrarTarefa();
+}
+
+function deletarTarefa(posicao){
+    minhaListaDeItens.splice(posicao, 1)
+
+    mostrarTarefa();
+}
+
+function recarregarTarefa(){
+    const TarefaNoLocalStorage = localStorage.getItem('lista')
+
+    if(TarefaNoLocalStorage){
+        minhaListaDeItens= JSON.parse(TarefaNoLocalStorage)
+    }
+
+    mostrarTarefa()
+}
+
+recarregarTarefa()
+botao.addEventListener('click', adicionarNovaTarefa)
